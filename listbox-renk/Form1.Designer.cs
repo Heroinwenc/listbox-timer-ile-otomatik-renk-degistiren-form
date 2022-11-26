24cb5747c511291d71cccbb9d0062cab719dc472
ogrenci <ogrenci@C01>
2022-11-26 11:28:24 +0300

    Add project files.

---
+++ listbox-renk/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace listbox_renk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Sayac = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Mavi",
+            "Yeşil",
+            "Kırmızı",
+            "Sarı",
+            "Mor",
+            "Gri"});
+            this.listBox1.Location = new System.Drawing.Point(23, 26);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 121);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Renk Seç;";
+            // 
+            // Sayac
+            // 
+            this.Sayac.Enabled = true;
+            this.Sayac.Interval = 1000;
+            this.Sayac.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Renk";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 181);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form Renklendirme";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer Sayac;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ listbox-renk/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace listbox_renk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           listBox1.SelectedIndex = rnd.Next(0, 6);
+           label2.Text = listBox1.SelectedItem + " Seçildi";
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (listBox1.Text)
+            {
+                case "Mavi":
+                    {
+                        this.BackColor = Color.Blue;
+                        break;
+                    }
+                case "Yeşil":
+                    {
+                        this.BackColor = Color.Green;
+                        break;
+                    }
+                case "Kırmızı":
+                    {
+                        this.BackColor = Color.Red;
+                        break;
+                    }
+                case "Sarı":
+                    {
+                        this.BackColor = Color.Yellow;
+                        break;
+                    }
+                case "Mor":
+                    {
+                        this.BackColor = Color.Purple;
+                        break;
+                    }
+                case "Gri":
+                    {
+                        this.BackColor = Color.Gray;
+                        break;
+                    }
+            }
+        }
+    }
+}
+/**/
